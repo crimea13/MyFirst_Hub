b35f05adaec06549cf82ad2913a1ab97b77fe40e
crimea13 <crimea13@gmail.com>
2024-08-19 15:48:59 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,22 @@
+number_completed_tasks = 12
+number_hours = 1.5
+course_name = "Python"
+time_for_task = number_hours/number_completed_tasks
+print(course_name, ", всего задач: ", number_completed_tasks, ", затрачено часов: ",\
+      number_hours, ", среднее время выполнения ", time_for_task, ".")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
