5c43c6f63906eb4c055bcc2936626e2aeb0185d8
crimea13 <crimea13@gmail.com>
2024-08-24 18:02:38 +0300

    Create task_1.py

---
+++ task_1.py
@@ -0,0 +1,16 @@
+from networkx.algorithms.tournament import score_sequence
+
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students_list = list(students)
+#print(students_list)
+students_sortlist = sorted(students_list)
+#print(students_sortlist)
+score_student = []
+for i in range(len(grades)):
+    s = sum(grades[i])/len(grades[i])
+    #print(s)
+    score_student.append(s)
+#print(score_student)
+score_students = dict(zip(students_sortlist, score_student))
+print(score_students)
