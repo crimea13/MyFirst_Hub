f7ce99c88c997a58557e1389f919a2c3c2693c44
crimea13 <crimea13@gmail.com>
2024-08-31 16:06:32 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,27 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+    return calls
+
+def string_info(string):
+    count_calls()
+    tutle_1 = (len(string), string.upper(), string.lower())
+    return tutle_1
+
+def is_contains(string, list_to_search):
+    count_calls()
+    new_string = string.lower()
+    new_list = [x.lower() for x in list_to_search]
+    if new_string in new_list:
+        return True
+    else:
+        return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
+
