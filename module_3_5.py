9ef697a328059bace6a07ad6ff8a229081b11619
crimea13 <crimea13@gmail.com>
2024-09-14 17:34:41 +0300

    mhgkjvh gy

--- module_3_5.py
+++ module_3_5.py
@@ -6,4 +6,4 @@ def get_multiplied_digits(number):
     else:
         return first * get_multiplied_digits(int(str_number[1:]))
 
-print(get_multiplied_digits(41960634))
+print(get_multiplied_digits(40203))
